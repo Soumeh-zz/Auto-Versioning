656a218802b5dc626fa9ead7e5852f4b0cae5ea9
Soumeh <stefi.brezovac@gmail.com>
2021-07-11 15:02:35 +0200

    try it now

--- script.py
+++ script.py
@@ -4,8 +4,22 @@ from urllib.error import HTTPError
 from json import loads
 
 token = getenv('TOKEN', '""')
-commits = loads(getenv('COMMITS', '[]'))
+payload = loads(getenv('PAYLOAD', '{"commits": []}'))
 repo = loads(getenv('REPO', '{}'))
+
+def get_commits(url: str, commits: list) -> list:
+    try:
+        before = commits[0]['timestamp']
+        after = commits[-1]['timestamp']
+    except IndexError:
+        return []
+    headers = {'Authorization': 'token '+token}
+    rq = Request(url, headers=headers)
+    try:
+        json = urlopen(rq, data=bytes(f'{"accept": "application/vnd.github.v3+json", "since": "{before}", "until", "{after}"}', encoding='utf8')).read().decode('utf-8')
+        return loads(json.json())
+    except HTTPError:
+        return []
 
 def check_if_major(commits: list) -> bool:
     for commit in commits:
@@ -22,11 +36,8 @@ def get_latest_tag(url: str) -> str:
     except HTTPError:
         return None
 
-def gen_new_tag(tag: str) -> str:
+def gen_new_tag(major: bool, tag: str) -> str:
     major = check_if_major(commits)
-    tag = get_latest_tag(f'https://api.github.com/repos/{repo}/tags')
-    if not tag:
-        return getenv('FALLBACK_TAG', '0.0')
     tag = ''.join([i for i in tag if i.isnumeric() or i == '.']).split('.')
     tag = [int(i) for i in tag]
     if major:
@@ -44,10 +55,13 @@ def gen_new_tag(tag: str) -> str:
     return '.'.join(tag)
 
 if __name__ == '__main__':
-    print(token)
-    print(commits)
-    print(repo)
+    commits = get_commits(f'https://api.github.com/repos/{repo}/commits', payload['commits'])
+    if not commits:
+        commits = payload['commits']
     major = check_if_major(commits)
     tag = get_latest_tag(f'https://api.github.com/repos/{repo}/tags')
-    tag = gen_new_tag(tag)
+    if not tag:
+        tag = getenv('FALLBACK_TAG', '0.0')
+    else:
+        tag = gen_new_tag(major, tag)
     print('::set-output name=tag::'+tag)
