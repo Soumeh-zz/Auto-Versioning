4288a9061d4f5fad0f344866fe3c9b7f3ca3341c
Soumeh <stefi.brezovac@gmail.com>
2021-07-14 01:28:53 +0200

    get every change ever, now

--- script.py
+++ script.py
@@ -1,10 +1,9 @@
 from os import getenv
 from requests import get
-from json import load
+from json import loads
 
 token = getenv('TOKEN', '')
-before = getenv('BEFORE', '')
-after = getenv('AFTER', '')
+commits = loads(getenv('COMMITS', '[]'))
 repo = getenv('REPO', '')
 
 def parse_changes(files: list) -> bool:
@@ -16,6 +15,8 @@ def parse_changes(files: list) -> bool:
             if status == 'renamed' or status == 'added':
                 major = True
         filename = file['filename']
+        if filename in changelog['deleted']:
+            continue
         if status == 'renamed':
             changelog['renamed'].append('`'+file['previous_filename']+'` → `'+filename+'`')
         if status == 'added':
@@ -48,7 +49,10 @@ def gen_new_tag(major: bool, tag: str) -> str:
     return '.'.join([str(i) for i in tag])
 
 if __name__ == '__main__':
-    files = get_data(f'https://api.github.com/repos/{repo}/compare/{before}...{after}')['files']
+    commit_ids = [commit['id'] for commit in commits]
+    files = []
+    for commit_id in commit_ids:
+        files.append(get_data(f'https://api.github.com/repos/{repo}/commits/{commit_id}')['files'])
     changelog, major = parse_changes(files)
     try:
         tag = get_data(f'https://api.github.com/repos/{repo}/tags')[0]['name']
